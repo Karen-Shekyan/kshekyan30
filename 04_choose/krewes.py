bb167b287b9f2652760aea0632e29b6a2f0b193b
Karen Shekyan <kshekyan30@stuy.edu>
2022-09-28 12:57:02 -0400

    git bugs?

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -11,6 +11,8 @@ DISCO:
  - random.randint is inclusive on BOTH endpoints
  - .keys() returns a dict_keys object, which is not subscriptable (can't get element by index)
     - dict_keys can be type-cast into a list with "list(dict_keys)"
+ - Try-catch statements in python! They function similar to java, and the syntax is
+
 
 QCC:
  - Usually ranges are exclusive at the end, why is randint inclusive?
@@ -27,14 +29,51 @@ OPS SUMMARY:
 
 import random
 
-krewes = {2:["A", "B", "C"], 3:["D", "E", "F"]}
-
-def getRandKrew():
+def getRandKrew(krewes):
     keys = list(krewes.keys())
     keyIndex = random.randint(0, len(keys)-1)
     devos = krewes.get(keys[keyIndex])
     devosIndex = random.randint(0, len(devos)-1)
-    return devos[devosIndex]
+    return (devos[devosIndex], keys[keyIndex])
+
+#########################################################
+######################### TESTS #########################
+#########################################################
+
+krewes1 = {2:["A", "B", "C"], 3:["D", "E", "F"]}
+krewes2 = {
+    2:["NICHOLAS",  "ANTHONY",  "BRIAN",  "SAMUEL",  "JULIA",  "YUSHA",  "CORINA",  "CRAIG",  "FANG MIN",  "JEFF",  "KONSTANTIN",  "AARON",  "VIVIAN",  "AYMAN",  "TALIA",  "FAIZA",  "ZIYING",  "YUK KWAN",  "DANIEL",  "WEICHEN",  "MAYA",  "ELIZABETH",  "ANDREW",  "VANSH",  "JONATHAN",  "ABID",  "WILLIAM",  "HUI",  "ANSON",  "KEVIN",  "DANIEL",  "IVAN",  "JASMINE",  "JEFFREY", "Ruiwen"],
+    7:["DIANA",  "DAVID",  "SAM",  "PRATTAY",  "ANNA",  "JING YI",  "ADEN",  "EMERSON",  "RUSSELL",  "JACOB",  "WILLIAM",  "NADA",  "SAMANTHA",  "IAN",  "MARC",  "ANJINI",  "JEREMY",  "LAUREN",  "KEVIN",  "RAVINDRA",  "SADI",  "EMILY",  "GITAE",  "MAY",  "MAHIR",  "VIVIAN",  "GABRIEL",  "BRIANNA",  "JUN HONG",  "JOSEPH",  "MATTHEW",  "JAMES",  "THOMAS",  "NICOLE",  "Karen"],
+    8:["ALEKSANDRA",  "NAKIB",  "AMEER",  "HENRY",  "DONALD",  "YAT LONG",  "SEBASTIAN",  "DAVID",  "YUKI",  "SHAFIUL",  "DANIEL",  "SELENA",  "JOSEPH",  "SHINJI",  "RYAN",  "APRIL",  "ERICA",  "JIAN HONG",  "VERIT",  "JOSHUA",  "WILSON",  "AAHAN",  "GORDON",  "JUSTIN",  "MAYA",  "FAIYAZ",  "SHREYA",  "ERIC",  "JEFFERY",  "BRIAN",  "KEVIN",  "SAMSON",  "BRIAN",  "HARRY",  "Wanying", "Kevin"]
+}
+krewes3 = {}
+krewes4 = {4:[], 5:[], 6:[]}
 
 for i in range(20):
-    print(getRandKrew())
+    try:
+        (devo,period) = getRandKrew(krewes1)
+        print(str(devo) + " from period " + str(period))
+    except ValueError:
+        print("ERROR: Dictionary has length 0")
+print("\n")
+
+for i in range(20):
+    try:
+        (devo,period) = getRandKrew(krewes2)
+        print(str(devo) + " from period " + str(period))
+    except ValueError:
+        print("ERROR: Dictionary has length 0")
+print("\n")
+
+for i in range(1):
+    try:
+        (devo,period) = getRandKrew(krewes3)
+        print(str(devo) + " from period " + str(period))
+    except ValueError:
+        print("ERROR: Dictionary has length 0")
+print("\n")
+
+for i in range(20):
+    try:
+        (devo,period) = getRandKrew(krewes4)
+        print(str(devo) + " from period " + str(period))
